deabf0ffecb5ff200b91461559bdbb6abc73f89a
Whirang-Kim <wkim27@hawk.iit.edu>
2018-11-20 15:04:07 -0600

    edit code

---
+++ Javascript/map.js
@@ -0,0 +1,31 @@
+function initMap(){
+  var koreatech = {lat: 36.764512, lng: 127.282556};
+  var campus1 = {lat: 36.763691, lng: 127.281916};
+  var campus2 = {lat: 36.765556, lng: 127.280575};
+  var dorm = {lat:36.761508, lng: 127.283150};
+  var map = new google.maps.Map(
+    document.getElementById('map'), {
+    zoom: 12.5, center: bridge
+    }
+ );
+ 
+ var first = new google.maps.Marker({
+    position: koreatech,
+    map: map
+  });
+
+  var second = new google.maps.Marker({
+    position: campus1,
+    map: map
+  });
+
+  var third = new google.maps.Marker({
+    position: campus2,
+    map: map
+  });
+  
+  var fourth = new google.maps.Marker{{
+    position: dorm,
+	map: map
+  });
+}
